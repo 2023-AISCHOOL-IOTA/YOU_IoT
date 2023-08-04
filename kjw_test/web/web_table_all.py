cee088581d0c1e4ffc91866d80a900c5c2093f4a
김주완 <kjwane@naver.com>
2023-08-04 09:53:03 +0900

    웹서버에 html_css적용

---
+++ kjw_test/web/web_table_all.py
@@ -0,0 +1,64 @@
+# web 페이지에 주차장 보여주기
+from flask import Flask, jsonify, render_template
+import mysql.connector
+
+app = Flask(__name__)
+
+app.config["MYSQL_HOST"] = "project-db-stu3.smhrd.com"  # MySQL 호스트 주소
+app.config["MYSQL_USER"] = "Insa4_IOTA_hacksim_3"  # MySQL 사용자 이름
+app.config["MYSQL_PASSWORD"] = "aishcool3"  # MySQL 비밀번호
+app.config["MYSQL_DB"] = "Insa4_IOTA_hacksim_3"  # 사용할 데이터베이스 이름
+
+mysql_conn = mysql.connector.connect(
+    host=app.config["MYSQL_HOST"],
+    user=app.config["MYSQL_USER"],
+    password=app.config["MYSQL_PASSWORD"],
+    database=app.config["MYSQL_DB"],
+    port = 3307
+)
+
+# 라우트 정의: 데이터베이스에서 값을 가져와 JSON 응답으로 내보냅니다.
+@app.route("/", methods=["GET"])
+def home():
+    
+    return render_template("webserver.html")
+
+##### web_table_parking
+@app.route("/parking", methods=["GET"])
+def parking():
+     # 커서 객체 생성
+    cursor = mysql_conn.cursor()
+
+    # 예시로 모든 레코드를 조회하고 결과를 리스트로 만들어 JSON으로 반환합니다.
+    query = "SELECT * FROM parking;"  # your_table_name에는 사용할 테이블 이름을 넣으세요.
+    cursor.execute(query)
+    
+    data = cursor.fetchall()
+    cursor.close()
+
+    # index.html 템플릿 렌더링하며 데이터 전달
+    return render_template("CSS_parking.html", data=data)
+
+##### web_holiday_parking
+@app.route("/holiday", methods=["GET"])
+def holiday():
+    cursor = mysql_conn.cursor()
+
+    query = "SELECT * FROM holiday;"
+    cursor.execute(query)
+    
+    # 커서와 연결 닫기
+    # cursor.close()
+    data = cursor.fetchall()
+    num1 = data[0]
+    cursor.close()
+
+    return render_template("CSS_holiday.html", data=data)
+    
+@app.route("/servor3", methods=["GET"])
+def street_park():
+    
+    return render_template("CSS_street_park.html")
+
+if __name__ == "__main__":
+    app.run(host = "localhost", port = 8080, debug=True)
